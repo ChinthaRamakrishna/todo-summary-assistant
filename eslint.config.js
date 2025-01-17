0999569231f8cbc84cc9e6300b010a81401bf309
Eric Sun <ericwwsun@gmail.com>
2025-01-17 07:59:23 -0500

    chore: clean up eslint config and remove unused packages

--- eslint.config.js
+++ eslint.config.js
@@ -1,37 +1,26 @@
+import typescript from '@typescript-eslint/eslint-plugin'
 import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import globals from 'globals'
-import tseslint from 'typescript-eslint'
+import tsParser from '@typescript-eslint/parser'
 
-export default tseslint.config(
+export default [
   {
-    files: ['**/*.{js,jsx,ts,tsx}'],
-    extends: [
-      ...tseslint.configs.recommended,
-    ],
-    ignores: ['dist', 'node_modules', 'src/components/ui/*'],
+    files: ['src/**/*.{ts,tsx}'],
+    ignores: ['**/dist/**', '**/node_modules/**', 'src/components/ui/**'],
     languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-      globals: {
-        ...globals.browser,
-        ...globals.es2021,
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
       },
     },
     plugins: {
+      '@typescript-eslint': typescript,
       'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
     },
     rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
       'react-hooks/rules-of-hooks': 'error',
       'react-hooks/exhaustive-deps': 'warn',
-      'react-refresh/only-export-components': ['warn', { 
-        allowConstantExport: true,
-        allowExportNames: ['useAuth', 'AuthProvider']
-      }],
-      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-      '@typescript-eslint/no-explicit-any': 'warn',
-      '@typescript-eslint/no-non-null-assertion': 'off',
     },
   },
-)
+];
